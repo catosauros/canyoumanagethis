5a2d0aed2b94f316374f4cabbe80ae2aae4216e7
maria <maria.terminasyan@gmail.com>
2025-04-27 22:50:09 +0200

    scrolling is now pretty

---
+++ script.js
@@ -0,0 +1,40 @@
+let sections = document.querySelectorAll(".pagesection");
+let buttons = document.querySelectorAll(".butn");
+
+console.log(buttons);
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const id = entry.target.id; // Get the section id (like "section1")
+
+      let color = "";
+    
+      buttons.forEach(button => {
+        button.style.background = "transparent";
+      });
+      // Now set color based on section ID manually
+      if (id === "first") {
+        color="white";
+        buttons[0].style.background = color;
+      } else if (id === "second") {
+        color="black";
+        buttons[1].style.background = color;
+      } else if (id === "third") {
+        color="blue";
+        buttons[2].style.background = color;
+      }
+
+      buttons.forEach(button => {
+        button.style.borderColor = color;
+      });
+
+      
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
